2558bfe535706c08a70c90d21117a23608899b5d
Caleb O'Malley <calebomalley14@gmail.com>
2024-12-17 12:14:16 -0500

    add, delete, and read functionality

--- library.js
+++ library.js
@@ -1,4 +1,4 @@
-const myLibrary = [
+let myLibrary = [
     {
         title: 'Norwegian Wood',
         author: 'Haruki Murakami',
@@ -26,17 +26,21 @@ function Book(title, author, length, isRead) {
     this.isRead = isRead;
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    displayLibrary();
-}
-
 const libraryElement = document.querySelector('#library');
 
 function displayLibrary() {
-    myLibrary.forEach(book => {
-        console.log(book.title);
 
+    libraryElement.innerHTML = '';
+    let counter = 0;
+    
+    if (myLibrary.length == 0) {
+        const emptyText = document.createElement('h2');
+        emptyText.textContent = "Your library is empty! Try adding a book.";
+        libraryElement.appendChild(emptyText);
+    }
+    
+    myLibrary.forEach(book => {
+        
         const bookRoot = document.createElement('div');
         bookRoot.classList = "book-entry";
 
@@ -44,27 +48,53 @@ function displayLibrary() {
         const authorText = document.createElement('p');
         const lengthText = document.createElement('p');
         const isReadText = document.createElement('p');
+        const deleteBtn = document.createElement('button');
+        const readBtn = document.createElement('button');
+        const bookInfoDiv = document.createElement('div');
+        const buttonsDiv = document.createElement('div');
 
         titleText.textContent = book.title;
         authorText.textContent = `By: ${book.author}`;
         lengthText.textContent = `Pages: ${book.length}`;
         isReadText.textContent = book.isRead ? 'You have read this book.' : 'You have not read this book.';
+        deleteBtn.textContent = 'Delete';
+        readBtn.textContent = 'Read Book';
 
-        bookRoot.appendChild(titleText);
-        bookRoot.appendChild(authorText);
-        bookRoot.appendChild(lengthText);
-        bookRoot.appendChild(isReadText);
-
+        deleteBtn.className = 'delete-btn';
+        readBtn.className = 'read-btn';
+        bookInfoDiv.className = "book-info";
+        buttonsDiv.className = "book-actions";
+        
+        bookInfoDiv.appendChild(titleText);
+        bookInfoDiv.appendChild(authorText);
+        bookInfoDiv.appendChild(lengthText);
+        bookInfoDiv.appendChild(isReadText);
+        
+        buttonsDiv.appendChild(deleteBtn);
+        buttonsDiv.appendChild(readBtn);
+        
+        bookRoot.appendChild(bookInfoDiv);
+        bookRoot.appendChild(buttonsDiv);
+        
+        buttonsDiv.dataset.index = counter;
         libraryElement.appendChild(bookRoot);
+
+        document.querySelectorAll(".delete-btn").forEach(btn => {
+            btn.addEventListener("click", deleteBook);
+        });
+        document.querySelectorAll(".read-btn").forEach(btn => {
+            btn.addEventListener("click", readBook);
+        });
+
+        counter++;
     });
 }
 
 displayLibrary();
 
-// button logic
-const addBook = document.querySelector('#add-btn');
+// event logic
+const addBtn = document.querySelector('#add-btn');
 const cancelBtn = document.querySelector('#cancel-btn');
-const submitBook = document.querySelector("#submit-book");
 const form = document.querySelector("#add-form");
 
 function openForm() {
@@ -76,8 +106,38 @@ function closeForm() {
     form.style.visibility = "hidden";
     form.style.position = "absolute";
 }
-addBook.addEventListener("click", openForm);
+
+function submitForm(event) {
+    event.preventDefault(); // Prevent the default form submission
+  
+    const formData = new FormData(form);
+    
+    // Access the form data
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const length = formData.get("pages");
+    const isRead = formData.get("isRead") == "on" ? true : false;
+    
+    myLibrary.unshift(new Book(title, author, length, isRead));
+    displayLibrary();
+
+    form.reset();
+}
+
+function deleteBook() {
+    myLibrary.splice(this.parentElement.dataset.index, 1);
+    displayLibrary();
+}
+
+function readBook() {
+    myLibrary[this.parentElement.dataset.index].isRead = true;
+    displayLibrary();
+}
+
+addBtn.addEventListener("click", openForm);
 cancelBtn.addEventListener("click", closeForm);
+form.addEventListener("submit", submitForm);
+
 
 
 
